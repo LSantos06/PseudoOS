c9e4c1c7937f3f818d3d7bba09e23d68d1d18db4
Filipe <filipetc.bsb@gmail.com>
2017-12-03 03:11:07 +0000

    + Gerenciador de Arquivos funcional (quase completo -> faltam comentarios/ajustes finos)

--- arquivos.cpp
+++ arquivos.cpp
@@ -1,9 +1,519 @@
-#include "arquivos.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"arquivos.h"
 
-void Arquivo::foo(){
+// 
+/*int main(int argc, char *argv[])
+{
+    Disco *disco = montarDisco(argv[1]);
+
+    imprimirDisco(disco);
+    printf("\n");
+    printf("%d) ",realizarOperacao(disco, 1, 0));    imprimirBlocos(disco);
+    printf("%d) ",realizarOperacao(disco, 1, 0));    imprimirBlocos(disco);
+    printf("%d) ",realizarOperacao(disco, 0, 0));    imprimirBlocos(disco);
+    printf("%d) ",realizarOperacao(disco, 1, 0));    imprimirBlocos(disco);
+    printf("\n");
+    imprimirDisco(disco);
+    printf("\n");
+    desmontarDisco(disco);
+
+    return 0;
+}*/
+
+// 
+int main(int argc, char *argv[])
+{
+    Disco *disco = montarDisco(argv[1]);
+
+    imprimirDisco(disco);
+    printf("\n");
+    printf("%d) ",realizarOperacoes(disco, 1, 0));    imprimirBlocos(disco);
+    printf("%d) ",realizarOperacoes(disco, 0, 0));    imprimirBlocos(disco);
+    printf("\n");
+    imprimirDisco(disco);
+    printf("\n");
+    desmontarDisco(disco);
+
+    return 0;
+}
+
+// 
+
+// 
+Disco* montarDisco(char *entrada)
+{
+    // 
+    FILE *fp = fopen(entrada,"r");
+    if(fp == NULL)
+    {
+        printf("Erro ao abrir arquivo '%s'.\n",entrada);
+        return NULL;
+    }
+    Disco *disco = (Disco *)malloc(sizeof(Disco));
+    ListaArquivo auxListaArquivo;
+    int nRead = 0, auxInt1 = -1, auxInt2 = 0;
+    ListaOperacao auxListaOperacao;
+    char buffer[40] = {0};
+    char *pch;
+
+    // 
+    disco->nBlocos = -1;
+    disco->nSegmentosOcupados = -1;
+    disco->arquivos = criarListaArquivo();
+    disco->operacoes = criarListaOperacao();
+    disco->realizadas = criarListaOperacao();
+
+    // 
+    while(!feof(fp))
+    {
+        // 
+        do
+        {
+            auxInt1++;
+            nRead = fread(&buffer[auxInt1],sizeof(char),1,fp);
+        }
+        while(buffer[auxInt1] != '\n' && nRead != 0);
+        buffer[auxInt1] = '\0';
+        auxInt1 = -1;
+        // 
+        if(!feof(fp))
+        {
+            // 
+            if(disco->nBlocos == -1)
+                disco->nBlocos = atoi(buffer);
+            // 
+            else if(disco->nSegmentosOcupados == -1)
+                disco->nSegmentosOcupados = atoi(buffer);
+            // 
+            else if(auxInt2 < disco->nSegmentosOcupados)
+            {
+                auxInt2++;
+                // 
+                pch = strtok(buffer," ");
+                auxListaArquivo.nomeArquivo = *pch;
+                // 
+                pch = strtok(NULL," ");
+                pch[strlen(pch)-1] = '\0';
+                auxListaArquivo.nPrimeiroBloco = atoi(pch);
+                // 
+                pch = strtok(NULL,"\n");
+                auxListaArquivo.nBlocosOcupados = atoi(pch);
+                // 
+                disco->arquivos = inserirListaArquivo(disco->arquivos,auxListaArquivo.nomeArquivo,auxListaArquivo.nPrimeiroBloco,auxListaArquivo.nBlocosOcupados,-1);
+            }
+            // 
+            else
+            {
+                // 
+                pch = strtok(buffer," ");
+                pch[strlen(pch)-1] = '\0';
+                auxListaOperacao.idProcesso = atoi(pch);
+                // 
+                pch = strtok(NULL," ");
+                pch[strlen(pch)-1] = '\0';
+                auxListaOperacao.codigoOperacao = atoi(pch);
+                // 
+                if(auxListaOperacao.codigoOperacao)
+                {
+                    pch = strtok(NULL,"\n");
+                    auxListaOperacao.nomeArquivo = *pch;
+                    auxListaOperacao.nBlocosCriar = 0;
+                }
+                // 
+                else
+                {
+                    // 
+                    pch = strtok(NULL," ");
+                    auxListaOperacao.nomeArquivo = *pch;
+                    // 
+                    pch = strtok(NULL,"\n");
+                    auxListaOperacao.nBlocosCriar = atoi(pch);
+                }
+                // 
+                disco->operacoes = inserirListaOperacao(disco->operacoes,auxListaOperacao.idProcesso,auxListaOperacao.codigoOperacao,auxListaOperacao.nomeArquivo,auxListaOperacao.nBlocosCriar, 0);
+            }
+        }
+    }
+    fclose(fp);
+    return disco;
+}
+
+// 
+void desmontarDisco(Disco *disco)
+{
+    ListaOperacao *auxListaOperacao = NULL;
+
+    desalocarListaArquivo(disco->arquivos);
+    for(auxListaOperacao=disco->realizadas; auxListaOperacao != NULL; auxListaOperacao = auxListaOperacao->proxima)
+    {
+        switch(auxListaOperacao->estado)
+        {
+            case CRIAR_SUCESSO:
+                printf("O processo %d criou o arquivo %c.\n", auxListaOperacao->idProcesso, auxListaOperacao->nomeArquivo);
+                break;
+            case CRIAR_FALHA:
+                printf("O processo %d nao pode criar o arquivo %c.\n", auxListaOperacao->idProcesso, auxListaOperacao->nomeArquivo);
+                break;
+            case DELETAR_SUCESSO:
+                printf("O processo %d deletou o arquivo %c.\n", auxListaOperacao->idProcesso, auxListaOperacao->nomeArquivo);
+                break;
+            case DELETAR_FALHA:
+                printf("O processo %d nao pode deletar o arquivo %c.\n", auxListaOperacao->idProcesso, auxListaOperacao->nomeArquivo);
+                break;
+            default:
+                break;
+        }
+    }
+    desalocarListaOperacao(disco->realizadas);
+    for(auxListaOperacao=disco->operacoes; auxListaOperacao != NULL; auxListaOperacao = auxListaOperacao->proxima)
+    {
+        printf("O processo %d nao foi executado.\n", auxListaOperacao->idProcesso);
+    }
+    desalocarListaOperacao(disco->operacoes);
+    free(disco);
     return;
 }
 
-void Operacao::foo(){
+// 
+void imprimirDisco(Disco *disco)
+{
+    printf("nBlocos: %d\nnSegmentosOcupados: %d\n",disco->nBlocos,disco->nSegmentosOcupados);
+    printf("arquivos:\n");
+    imprimirListaArquivo(disco->arquivos);
+    printf("operacoes:\n");
+    imprimirListaOperacao(disco->operacoes);
+    printf("realizadas:\n");
+    imprimirListaOperacao(disco->realizadas);
+    return;
+}
+
+// 
+
+// 
+int realizarOperacao(Disco *disco, int idProcesso, int prioridadeProcesso)
+{
+    ListaOperacao *auxListaOperacao = NULL, *antListaOperacao = NULL;
+    int auxInt = 0;
+
+    for(auxListaOperacao = disco->operacoes; auxListaOperacao != NULL; auxListaOperacao = auxListaOperacao->proxima)
+    {
+        if(auxListaOperacao->idProcesso == idProcesso)
+        {
+            // 
+            if(antListaOperacao != NULL)
+                antListaOperacao->proxima = auxListaOperacao->proxima;
+            else
+                disco->operacoes = auxListaOperacao->proxima;
+            // 
+            if(auxListaOperacao->codigoOperacao == 0)
+                auxInt = criarArquivo(disco,auxListaOperacao->nomeArquivo,auxListaOperacao->nBlocosCriar,idProcesso);
+            else
+                auxInt = deletarArquivo(disco,auxListaOperacao->nomeArquivo,idProcesso,prioridadeProcesso);
+            // 
+            disco->realizadas = inserirListaOperacao(disco->realizadas,auxListaOperacao->idProcesso,auxListaOperacao->codigoOperacao,auxListaOperacao->nomeArquivo,auxListaOperacao->nBlocosCriar,auxInt);
+            free(auxListaOperacao);
+            return auxInt;
+        }
+        antListaOperacao = auxListaOperacao;
+    }
+    return OPERACAO_NAO_REALIZADA;
+}
+
+// 
+int realizarOperacoes(Disco *disco, int idProcesso, int prioridadeProcesso)
+{
+    ListaOperacao *auxListaOperacao = NULL, *antListaOperacao = NULL;
+    ListaOperacao aux2ListaOperacao =
+    {
+        .idProcesso = 0,
+        .codigoOperacao = 0,
+        .nomeArquivo = (char)0,
+        .nBlocosCriar = 0,
+        .estado = 0,
+        .proxima = NULL
+    };
+    int auxInt = 0;
+
+    for(auxListaOperacao = disco->operacoes; auxListaOperacao != NULL; auxListaOperacao = auxListaOperacao->proxima)
+    {
+        if(auxListaOperacao->idProcesso == idProcesso)
+        {
+            // 
+            if(antListaOperacao != NULL)
+                antListaOperacao->proxima = auxListaOperacao->proxima;
+            else
+                disco->operacoes = auxListaOperacao->proxima;
+            // 
+            if(auxListaOperacao->codigoOperacao == 0)
+                auxInt = criarArquivo(disco,auxListaOperacao->nomeArquivo,auxListaOperacao->nBlocosCriar,idProcesso);
+            else
+                auxInt = deletarArquivo(disco,auxListaOperacao->nomeArquivo,idProcesso,prioridadeProcesso);
+            // 
+            disco->realizadas = inserirListaOperacao(disco->realizadas,auxListaOperacao->idProcesso,auxListaOperacao->codigoOperacao,auxListaOperacao->nomeArquivo,auxListaOperacao->nBlocosCriar, auxInt);
+            // 
+            if(antListaOperacao != NULL)
+            {
+                free(auxListaOperacao);
+                auxListaOperacao = antListaOperacao;
+            }
+            else
+            {
+                aux2ListaOperacao.proxima = auxListaOperacao->proxima;
+                free(auxListaOperacao);
+                auxListaOperacao = (ListaOperacao *)&aux2ListaOperacao;
+            }
+        }
+        else
+            antListaOperacao = auxListaOperacao;
+    }
+    return OPERACOES_REALIZADAS;
+}
+
+// 
+int criarArquivo(Disco *disco, char nomeArquivo, int nBlocosCriar, int idProcesso)
+{
+    ListaArquivo *auxListaArquivo = NULL;
+    ListaArquivo *novo = NULL;
+    int firstFlag = 1;
+
+    for(auxListaArquivo = disco->arquivos; auxListaArquivo != NULL; auxListaArquivo = auxListaArquivo->proximo)
+    {
+        if(firstFlag)
+        {
+            firstFlag = 0;
+            if(nBlocosCriar <= auxListaArquivo->nPrimeiroBloco)
+            {
+                novo = (ListaArquivo *)malloc(sizeof(ListaArquivo));
+                novo->nomeArquivo = nomeArquivo;
+                novo->nPrimeiroBloco = 0;
+                novo->nBlocosOcupados = nBlocosCriar;
+                novo->idProcesso = idProcesso;
+                novo->proximo = auxListaArquivo;
+                disco->arquivos = novo;
+                disco->nSegmentosOcupados++;
+                return CRIAR_SUCESSO;
+            }
+        }
+        if(auxListaArquivo->proximo != NULL && nBlocosCriar <= auxListaArquivo->proximo->nPrimeiroBloco - (auxListaArquivo->nPrimeiroBloco + auxListaArquivo->nBlocosOcupados))
+        {
+            novo = (ListaArquivo *)malloc(sizeof(ListaArquivo));
+            novo->nomeArquivo = nomeArquivo;
+            novo->nPrimeiroBloco = auxListaArquivo->nPrimeiroBloco + auxListaArquivo->nBlocosOcupados;
+            novo->nBlocosOcupados = nBlocosCriar;
+            novo->idProcesso = idProcesso;
+            novo->proximo = auxListaArquivo->proximo;
+            auxListaArquivo->proximo = novo;
+            disco->nSegmentosOcupados++;
+            return CRIAR_SUCESSO;
+        }
+        if(vazioListaArquivo(auxListaArquivo->proximo) && nBlocosCriar <= disco->nBlocos - (auxListaArquivo->nPrimeiroBloco + auxListaArquivo->nBlocosOcupados))
+        {
+            novo = (ListaArquivo *)malloc(sizeof(ListaArquivo));
+            novo->nomeArquivo = nomeArquivo;
+            novo->nPrimeiroBloco = auxListaArquivo->nPrimeiroBloco + auxListaArquivo->nBlocosOcupados;
+            novo->nBlocosOcupados = nBlocosCriar;
+            novo->idProcesso = idProcesso;
+            novo->proximo = auxListaArquivo->proximo;
+            auxListaArquivo->proximo = novo;
+            disco->nSegmentosOcupados++;
+            return CRIAR_SUCESSO;
+        }
+    }
+    return CRIAR_FALHA;
+}
+
+// 
+int deletarArquivo(Disco *disco, char nomeArquivo, int idProcesso, int prioridadeProcesso)
+{
+    ListaArquivo *auxListaArquivo = NULL, *antListaArquivo = NULL;
+
+    for(auxListaArquivo = disco->arquivos; auxListaArquivo != NULL; auxListaArquivo = auxListaArquivo->proximo)
+    {
+        if(auxListaArquivo->nomeArquivo == nomeArquivo)
+        {
+            if((!prioridadeProcesso) || (prioridadeProcesso && auxListaArquivo->idProcesso == idProcesso))
+            {
+                if(antListaArquivo != NULL)
+                    antListaArquivo->proximo = auxListaArquivo->proximo;
+                else
+                    disco->arquivos = auxListaArquivo->proximo;
+                free(auxListaArquivo);  
+                disco->nSegmentosOcupados--;
+                return DELETAR_SUCESSO;
+            }
+            else
+                return DELETAR_FALHA;
+        }
+        antListaArquivo = auxListaArquivo;
+    }
+    return DELETAR_FALHA;
+}
+
+// 
+void imprimirBlocos(Disco *disco)
+{
+    ListaArquivo *auxListaArquivo = NULL;
+    int auxInt = 0;
+    int firstFlag = 1;
+    
+    for(auxListaArquivo = disco->arquivos; auxListaArquivo != NULL; auxListaArquivo = auxListaArquivo->proximo)
+    {
+        if(firstFlag)
+        {
+            firstFlag = 0;
+            while(auxInt < auxListaArquivo->nPrimeiroBloco)
+            {
+                printf("|0");
+                auxInt++;
+            }   
+            auxInt = 0;
+        }
+        while(auxInt < auxListaArquivo->nBlocosOcupados)
+        {
+            printf("|%c",auxListaArquivo->nomeArquivo);
+            auxInt++;
+        }
+        auxInt = 0;
+        if(!vazioListaArquivo(auxListaArquivo->proximo))
+        {
+            while(auxInt < auxListaArquivo->proximo->nPrimeiroBloco - (auxListaArquivo->nPrimeiroBloco + auxListaArquivo->nBlocosOcupados))
+            {
+                printf("|0");
+                auxInt++;
+            }
+        }
+        else
+        {
+            while(auxInt < disco->nBlocos - (auxListaArquivo->nPrimeiroBloco + auxListaArquivo->nBlocosOcupados))
+            {
+                printf("|0");
+                auxInt++;
+            }
+            printf("|\n");
+        }
+        auxInt = 0;
+    }
+    return;
+}
+
+// 
+
+// 
+ListaArquivo* criarListaArquivo()
+{
+    return NULL;
+}
+
+// 
+ListaArquivo* inserirListaArquivo(ListaArquivo *arquivos, char nomeArquivo, int nPrimeiroBloco, int nBlocosOcupados, int idProcesso)
+{
+	ListaArquivo *auxListaArquivo = NULL;
+	ListaArquivo *novo = (ListaArquivo *)malloc(sizeof(ListaArquivo));
+
+    novo->nomeArquivo = nomeArquivo;
+    novo->nPrimeiroBloco = nPrimeiroBloco;
+    novo->nBlocosOcupados = nBlocosOcupados;
+    novo->idProcesso = idProcesso;
+    novo->proximo = NULL;
+    if(arquivos != NULL)
+    {
+        for(auxListaArquivo = arquivos; auxListaArquivo->proximo != NULL; auxListaArquivo = auxListaArquivo->proximo);
+        auxListaArquivo->proximo = novo;
+    }
+    else
+        arquivos = novo;
+    return arquivos;
+}
+
+// 
+int vazioListaArquivo(ListaArquivo *arquivos)
+{
+    if(arquivos == NULL)
+        return 1;
+    else
+        return 0;
+}
+
+// 
+void desalocarListaArquivo(ListaArquivo *arquivos)
+{
+    if(!vazioListaArquivo(arquivos))
+    {
+        desalocarListaArquivo(arquivos->proximo);
+        free(arquivos);
+    }
+    return;
+}
+
+// 
+void imprimirListaArquivo(ListaArquivo *arquivos)
+{
+    if(!vazioListaArquivo(arquivos))
+    {
+        printf("nomeArquivo: %c | nPrimeiroBloco: %d | nBlocosOcupados: %d | idProcesso: %d\n",arquivos->nomeArquivo,arquivos->nPrimeiroBloco,arquivos->nBlocosOcupados,arquivos->idProcesso);
+        imprimirListaArquivo(arquivos->proximo);
+    }
+    return;
+}
+
+// 
+
+// 
+ListaOperacao* criarListaOperacao()
+{
+    return NULL;
+}
+
+// 
+ListaOperacao* inserirListaOperacao(ListaOperacao *operacoes, int idProcesso, int codigoOperacao, char nomeArquivo, int nBlocosCriar, int estado)
+{
+	ListaOperacao *auxListaOperacao = NULL;
+	ListaOperacao *nova = (ListaOperacao *)malloc(sizeof(ListaOperacao));
+    nova->idProcesso = idProcesso;
+    nova->codigoOperacao = codigoOperacao;
+    nova->nomeArquivo = nomeArquivo;
+    nova->nBlocosCriar = nBlocosCriar;
+    nova->estado = estado;
+    nova->proxima = NULL;
+    if(operacoes != NULL)
+    {
+        for(auxListaOperacao = operacoes; auxListaOperacao->proxima != NULL; auxListaOperacao = auxListaOperacao->proxima);
+        auxListaOperacao->proxima = nova;
+    }
+    else
+        operacoes = nova;
+    return operacoes;
+}
+
+// 
+int vazioListaOperacao(ListaOperacao *operacoes)
+{
+    if(operacoes == NULL)
+        return 1;
+    else
+        return 0;
+}
+
+// 
+void desalocarListaOperacao(ListaOperacao *operacoes)
+{
+    if(!vazioListaOperacao(operacoes))
+    {
+        desalocarListaOperacao(operacoes->proxima);
+        free(operacoes);
+    }
+    return;
+}
+
+// 
+void imprimirListaOperacao(ListaOperacao *operacoes)
+{
+    if(!vazioListaOperacao(operacoes))
+    {
+        printf("idProcesso: %d | codigoOperacao: %d | nomeArquivo: %c | nBlocosCriar: %d | estado: %d\n",operacoes->idProcesso,operacoes->codigoOperacao,operacoes->nomeArquivo,operacoes->nBlocosCriar,operacoes->estado);
+        imprimirListaOperacao(operacoes->proxima);
+    }
     return;
 }
--- arquivos.h
+++ arquivos.h
@@ -1,20 +1,66 @@
-#include <iostream>
+// 
+typedef enum {
+    OPERACAO_NAO_REALIZADA = 0,
+    CRIAR_SUCESSO = 1,
+    CRIAR_FALHA = -1,
+    DELETAR_SUCESSO = 2,
+    DELETAR_FALHA = -2,
+    OPERACOES_REALIZADAS = 3
+} codigos;
 
-class Arquivo{
-public:    
-    char nome_arquivo;
-    int primeiro_bloco;
-    int qtd_blocos_ocupados;
-    void foo();
-        
-};
+// 
+typedef struct listaArquivo
+{
+    char nomeArquivo;
+    int nPrimeiroBloco;
+    int nBlocosOcupados;
+    int idProcesso;
+    struct listaArquivo *proximo;
+} ListaArquivo;
 
-class Operacao{
-public:
-    int id_processo;
-    int cod_operacao;
-    char nome_arquivo;
-    int numero_blocos;
-    void foo();
+// 
+typedef struct listaOperacao
+{
+    int idProcesso;
+    int codigoOperacao;
+    char nomeArquivo;
+    int nBlocosCriar;
+    int estado;
+    struct listaOperacao *proxima;
+} ListaOperacao;
+
+// 
+typedef struct disco
+{
+    int nBlocos;
+    int nSegmentosOcupados;
+    ListaArquivo *arquivos;
+    ListaOperacao *operacoes;
+    ListaOperacao *realizadas;
     
-};
+} Disco;
+
+// 
+Disco* montarDisco(char *);
+void desmontarDisco(Disco *);
+void imprimirDisco(Disco *);
+int realizarOperacao(Disco *, int, int);
+int realizarOperacoes(Disco *, int, int);
+int criarArquivo(Disco *, char, int, int);
+int deletarArquivo(Disco *, char, int, int);
+void imprimirBlocos(Disco *);
+
+// 
+ListaArquivo* criarListaArquivo();
+ListaArquivo* inserirListaArquivo(ListaArquivo *, char, int, int, int);
+int vazioListaArquivo(ListaArquivo *);
+void desalocarListaArquivo(ListaArquivo *);
+void imprimirListaArquivo(ListaArquivo *);
+
+// 
+ListaOperacao* criarListaOperacao(); 
+ListaOperacao* inserirListaOperacao(ListaOperacao *, int, int, char, int, int);
+int vazioListaOperacao(ListaOperacao *);
+void desalocarListaOperacao(ListaOperacao *);
+void imprimirListaOperacao(ListaOperacao *);
+
